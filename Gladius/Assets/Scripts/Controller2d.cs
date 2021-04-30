a4e18c0dbfae023df4b1fd0f51b9119fecc9cac1
Nixonm2 <maximeflageole@gmail.com>
2021-04-30 16:13:50 -0400

    Created a scene in 2d Adding collisions to the environment layer Adding assets Creating a quick char control to test collisions Things are aight so far

---
+++ Gladius/Assets/Scripts/Controller2d.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2d : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D m_rigidbody;
+    [SerializeField]
+    private float m_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vec2 = new Vector2();
+        if (Input.GetKey(KeyCode.W))
+        {
+            vec2 += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            vec2 += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vec2 += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            vec2 += Vector2.right;
+        }
+        vec2 *= m_speed;
+        m_rigidbody.velocity = vec2;
+    }
+}
